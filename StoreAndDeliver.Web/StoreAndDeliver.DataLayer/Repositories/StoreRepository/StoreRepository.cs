04ca84e99cbe25dc994d0872e391c53bd0bd40de
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-11-14 12:12:57 +0200

    Fix store updating

--- StoreAndDeliver.Web/StoreAndDeliver.DataLayer/Repositories/StoreRepository/StoreRepository.cs
+++ StoreAndDeliver.Web/StoreAndDeliver.DataLayer/Repositories/StoreRepository/StoreRepository.cs
@@ -2,6 +2,7 @@
 using StoreAndDeliver.DataLayer.DbContext;
 using StoreAndDeliver.DataLayer.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace StoreAndDeliver.DataLayer.Repositories.StoreRepository
@@ -15,6 +16,25 @@ namespace StoreAndDeliver.DataLayer.Repositories.StoreRepository
         public async Task<IEnumerable<Store>> GetStoresWithAddress()
         {
             return await context.Stores.Include(s => s.Address).AsNoTracking().ToListAsync();
+        }
+
+        public async Task UpdateStore(Store store)
+        {
+            var local = context.Set<Store>()
+                .Local
+                .FirstOrDefault(entry => entry.Id.Equals(store.Id));
+
+            // check if local is not null 
+            if (local != null)
+            {
+                // detach
+                context.Entry(local).State = EntityState.Detached;
+            }
+            // set Modified flag in your entry
+            context.Entry(store).State = EntityState.Modified;
+
+            // save 
+            await context.SaveChangesAsync();
         }
     }
 }
